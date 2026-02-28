b32cd6f12dc8fcd8a33cf5721ec21a68697e7c7e
kachiDeGreat <mail.ricx@gmail.com>
2026-02-28 19:18:57 +0100

    fixed style error

--- src/components/ui/pages/NotFound.tsx
+++ src/components/ui/pages/NotFound.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
-import styles from "../../styles/NotFound.module.css";
+import styles from "../styles/NotFound.module.css";
 import SEO from "../page-components/SEO";
 
 interface NotFoundProps {
